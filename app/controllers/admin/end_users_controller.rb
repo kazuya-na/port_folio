9ab25a839a6a4b730cfbf493c4b5cc261c397bd8
Kazuya Nagamachi <128442256+kazuya-na@users.noreply.github.com>
2023-06-17 05:19:39 +0000

    [Add]admin/end_user/show,editの追加、編集

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -9,8 +9,21 @@ class Admin::EndUsersController < ApplicationController
   end
 
   def edit
+    @end_user = EndUser.find(params[:id])
   end
 
   def update
+    @end_user = EndUser.find(params[:id])
+    if @end_user.update(end_user_params)
+      redirect_to admin_end_user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:nick_name, :gender, :date_of_birth, :introduction, :is_deleted, :email, :encrypted_password, :profile_image)
   end
 end
