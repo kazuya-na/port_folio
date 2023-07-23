a4aa1833c991bd9d52f38b98088081c5d45e19dd
Kazuya Nagamachi <128442256+kazuya-na@users.noreply.github.com>
2023-07-23 11:47:11 +0000

    [Fix]seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 unless Admin.find_by(email: 'admin@gmail.com')
   Admin.create!(
      email: 'admin@gmail.com',
